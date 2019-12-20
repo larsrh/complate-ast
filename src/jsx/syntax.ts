06e656687190d09f70e683f8f6a8aa2207d51f38
Lars Hupel <lars.hupel@innoq.com>
2019-12-20 15:08:50 +0100

    use own version of escapeHTML

--- src/jsx/syntax.ts
+++ src/jsx/syntax.ts
@@ -1,7 +1,11 @@
-// TODO use import ... from
-const escapeHtml = require("escape-html");
-
-export const escapeHTML: (s: string) => string = escapeHtml;
+export function escapeHTML(s: string): string {
+    return s
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#x27;");
+}
 
 export function isMacro(tag: string): boolean {
     const first = tag.charAt(0);
