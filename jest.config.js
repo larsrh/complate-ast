1be978969906e5fdaa7671b468d84b173f6a0064
Lars Hupel <lars.hupel@innoq.com>
2020-01-03 15:26:31 +0100

    run tests with Babel

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    testMatch: [
-        // jest default, except for ts -- those are built using tsc
-        "**/__tests__/**/*.js?(x)",
-        "**/?(*.)+(spec|test).js?(x)"
-    ],
-    verbose: true
+    verbose: true,
+    roots: ["<rootDir>/src"]
 };
