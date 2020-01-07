54fb65d8f2531893925000261fd30df6ab453cc0
Lars Hupel <lars.hupel@innoq.com>
2020-01-07 11:51:59 +0100

    more linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,28 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   plugins: [
-    '@typescript-eslint',
+      "@typescript-eslint",
+      "import"
   ],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript"
   ],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': ['error', {
-      allowExpressions: true
-    }],
-    'no-inner-declarations': 'off'
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/ban-ts-ignore": "off",
+      "@typescript-eslint/explicit-function-return-type": ["error", {
+          allowExpressions: true
+      }],
+      "no-inner-declarations": "off",
+      "import/no-unresolved": "off", // fails for 'estree' (types-only module)
+      "import/no-self-import": "error",
+      "import/no-cycle": "error"
   },
 };
