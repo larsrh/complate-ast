f4c49d577d7d0978bae248010dc97371f01a9235
Lars Hupel <lars.hupel@innoq.com>
2020-06-09 17:35:02 +0200

    export more symbols

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,24 @@ export * from "./runtime";
 
 export {HTMLString} from "./syntax/util";
 
-export {addItems, renderToString} from "./ast";
+export * from "./ast";
+
+import * as Base from "./ast/base";
+import * as Structured from "./ast/structured";
+import * as Raw from "./ast/raw";
+import * as Stream from "./ast/stream";
+
+export {Base, Structured, Raw, Stream};
 
 import complate from "./tools/rollup";
 
 export {complate};
 
 export {runtimeSymbols} from "./preprocess/runtime";
+
+export {ESTreeBuilder} from "./preprocess/estreebuilder"
+export * from "./preprocess/estreebuilders/config";
+
+export {defaultRuntimeConfig, runtimeModuleFromConfig, RuntimeModule} from "./preprocess/runtime";
+
+export {preprocess} from "./preprocess/preprocess";
